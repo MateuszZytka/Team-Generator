05501705e5c6a42a5181c1cb0a6b724af767fdd6
MateuszZytka <109117198+MateuszZytka@users.noreply.github.com>
2022-10-12 23:20:05 -0400

    changes title of webpage

--- Utils/GenerateManager.js
+++ Utils/GenerateManager.js
@@ -10,7 +10,7 @@ function generateManager({ managerName, managerID, email, officeNumber })  {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./CSS/style.css">
-    <title>Prework Study Guide</title>
+    <title>Team Generator</title>
   </head>
   <body>
   <header><h1>Team Members</h1></header>
